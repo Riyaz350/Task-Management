74417e6dbb04ea73c4f6e1c1e1e20d6dce17a4f2
riyaz <riyaz3582@gmail.com>
2023-12-20 23:39:39 +0600

    added create task page, installed axios, swal2, date picker

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Layout/Home/Home.jsx';
+import Dashboard from './Layout/Dashboard/Dashboard.jsx';
+import CreateTask from './Layout/Dashboard/ModifyTask/CreateTask.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    // children: [
+    //   {
+    //     path: "contacts/:contactId",
+    //     // element: <Contact />,
+    //   },
+    // ],
+  },
+  {
+    path:'/dashboard',
+    element:<Dashboard></Dashboard>,
+    children: [
+      {
+        path: "createTask",
+        element: <CreateTask></CreateTask>
+      },
+    ],
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
